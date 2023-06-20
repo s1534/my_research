1210d6575cba1bc1e8f26d95db895a962bcc5688
Yuki Mishima <41721281+s1534@users.noreply.github.com>
2023-06-20 22:58:30 +0900

    複数台に対応

--- src/multicamera.py
+++ src/multicamera.py
@@ -3,8 +3,7 @@ import open3d as o3d
 import keyboard
 import os
 import datetime
-from multiprocessing import Pool, Process
-
+from concurrent.futures import ProcessPoolExecutor
 
 def make_dirs(path):
     if not os.path.isdir(path):
@@ -12,9 +11,23 @@ def make_dirs(path):
         os.makedirs(os.path.join(path,'bag_data'))
         os.makedirs(os.path.join(path,'origin_ply'))
 
-def capture_bag(config_filename):
+def capture_bag(num,saving_dir):
+    if num == 1:
+        config_filename = 'src/config1.json'
+    if num == 2:
+        config_filename = 'src/config2.json'
+
     with open(config_filename) as cf:
         rs_cfg = o3d.t.io.RealSenseSensorConfig(json.load(cf))
+
+
+    time = str(datetime.datetime.now().time())[:8]
+
+    bagfile = 'bag_data/cooking' + str(num) + '-' + time + '.bag'
+    print('start capturing camera{}'.format(num))
+
+    bag_dir = os.path.join(saving_dir,bagfile)
+
 
     rs = o3d.t.io.RealSenseSensor()
     rs.init_sensor(rs_cfg, 0, bag_dir)
@@ -26,24 +39,17 @@ def capture_bag(config_filename):
         # if keyboard.read_key() == "q":
             # break
 
-# make folder
-today = datetime.date.today()
-today_ymd = today.strftime('%Y%m%d')
-time = str(datetime.datetime.now().time())[:8]
+if __name__ == "__main__":
+    print('----------------------------------')
+    # make folder
+    today = datetime.date.today()
+    today_ymd = today.strftime('%Y%m%d')
+    saving_dir = os.path.join('data',today_ymd)
+    make_dirs(saving_dir)
 
-config_filename_1 = 'src/config1.json'
-config_filename_2 = 'src/config2.json'
-
-p = Pool()
-
-result = p.apply_async(capture_bag, config_filename_1)
-result2 = p.apply_async(capture_bag, config_filename_2)
-
-saving_dir = os.path.join('data',today_ymd)
-make_dirs(saving_dir)
-
-bagfile = 'bag_data/cooking-'+time+'.bag'
-bag_dir = os.path.join(saving_dir,bagfile)
-
+    with ProcessPoolExecutor() as executor:
+        executor.submit(capture_bag,1,saving_dir)
+        executor.submit(capture_bag,2,saving_dir)
+    print('終了')
 
 
